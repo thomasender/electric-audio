946c9b582e43a4741aab74b2825d5d10a5966046
thomasender <freisingen@yahoo.com>
2023-11-08 16:49:33 +0100

    implement basic directory picker

--- public/electron.js
+++ public/electron.js
@@ -1,8 +1,11 @@
 const electron = require("electron");
 const path = require("path");
+const fs = require('fs');
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
+const dialog = electron.dialog;
 
 let mainWindow;
 
@@ -22,3 +25,23 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
+
+ipcMain.on('open-directory-dialog', (event) => {
+  dialog.showOpenDialog(mainWindow, {
+    properties: ['openDirectory'],
+  }).then(result => {
+    if (!result.canceled) {
+      const directoryPath = result.filePaths[0];
+      fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const mp3AndWavFiles = files.filter(file => path.extname(file).toLowerCase() === '.mp3' || path.extname(file).toLowerCase() === '.wav');
+          event.sender.send('selected-directory', directoryPath, mp3AndWavFiles);
+        }
+      });
+    }
+  }).catch(err => {
+    console.log(err);
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import { useState } from "react";
+import DirectoryPicker from "./components/directory-picker.tsx";
 
 function App() {
+  const [directory, setDirectory] = useState(null);
+  const [files, setFiles] = useState(null);
   return (
     <div>
       <h1>Electric Audio</h1>
+      <DirectoryPicker setDirectory={setDirectory} setFiles={setFiles} files={files} directory={directory} />
+      
     </div>
   );
 }
